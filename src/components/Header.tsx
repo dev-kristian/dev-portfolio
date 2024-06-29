3f9d485ba87c51faa4b3de119e57e17e12260849
Kristian <alex6346@hotmail.com>
2024-06-29 13:34:59 +0200

    changed name;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => (
   }}>
     <Toolbar>
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        Filan Fisteku
+        Kristian Aleksi
       </Typography>
     </Toolbar>
   </AppBar>
